f8345619d09edd2b13a9192faf2bc651ad8c887e
Anthony HAREL <a.harel@polysoude.com>
2024-05-06 14:03:14 +0200

    Fin lecon 2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'My beautiful socks'
         }
     }
 })
