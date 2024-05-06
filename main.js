8ac4a5db5ed0a62505b7817b0d6d4063df8cb55b
Anthony HAREL <a.harel@polysoude.com>
2024-05-06 14:59:07 +0200

    Fin lecon 5

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 8,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ]
         }
     }
 })
